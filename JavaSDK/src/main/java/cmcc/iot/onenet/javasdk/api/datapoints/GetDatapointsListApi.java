a6e6bee168320b308ae86e06f91f3c572f8fedf2
zhoujunhui239 <zhoujunhui239@163.com>
2017-09-05 14:33:24 +0800

    Update GetDatapointsListApi.java

--- JavaSDK/src/main/java/cmcc/iot/onenet/javasdk/api/datapoints/GetDatapointsListApi.java
+++ JavaSDK/src/main/java/cmcc/iot/onenet/javasdk/api/datapoints/GetDatapointsListApi.java
@@ -121,7 +121,7 @@ public class GetDatapointsListApi extends AbstractAPI{
 
 		} catch (Exception e) {
 			e.printStackTrace();
-			logger.error("error:" + e.getMessage());
+			logger.error("error: {}" e.getMessage());
 			throw new OnenetApiException();
 		}
 		return response;
