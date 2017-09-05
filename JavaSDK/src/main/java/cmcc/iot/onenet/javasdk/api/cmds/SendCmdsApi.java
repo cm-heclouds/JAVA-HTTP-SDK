6dc8e9298bcee4b38315f1b1fc0db233ca9e4e65
zhoujunhui239 <zhoujunhui239@163.com>
2017-09-05 14:32:17 +0800

    Update SendCmdsApi.java

--- JavaSDK/src/main/java/cmcc/iot/onenet/javasdk/api/cmds/SendCmdsApi.java
+++ JavaSDK/src/main/java/cmcc/iot/onenet/javasdk/api/cmds/SendCmdsApi.java
@@ -88,15 +88,13 @@ public class SendCmdsApi  extends AbstractAPI {
 			response.setData(newData);
 
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			logger.error("json error", e.getMessage());
+			logger.error("json error {}", e.getMessage());
 			throw new OnenetApiException();
 		}
 		try{
 			HttpMethod.httpClient.close();
 		}catch (Exception e) {
-			// TODO Auto-generated catch block
-			logger.error("http close error:" + e.getMessage());
+			logger.error("http close error: {}" , e.getMessage());
 			throw new OnenetApiException();
 		}
 		return response;
